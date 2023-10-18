b3807388dd34ddeac2c6de66afa21e10b88e5d8f
dani ferdinan <daniferdinandall@gmail.com>
2023-10-18 18:30:19 +0700

    update

--- js/postFunctions.js
+++ js/postFunctions.js
@@ -3,7 +3,8 @@ import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
 import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
 
 export default function PostSignUp() {
-    let target_url = "https://asia-southeast2-trens-project.cloudfunctions.net/trensentimen-post2";
+    let target_url = "https://asia-southeast2-trens-project.cloudfunctions.net/trensentimen-post";
+    // let target_url = "https://asia-southeast2-trens-project.cloudfunctions.net/trensentimen-post2";
     let tokenkey = "token";
     let tokenvalue = "f48bd58cb3b3972d05bb9303b15ce9b83f4fcb9c871d1b05906f2fec20620ea0";
     let datainjson = {
@@ -22,6 +23,7 @@ function responseData(result) {
     
     setInner("pesan", result.message);
     
-    // setCookieWithExpireHour("token", result.token, 2);
-    alert(result.message, result.token)
+    setCookieWithExpireHour("token", result.token, 2);
+    alert("Berhasil Masuk")
+    window.location.href = "dashboard.html";
 }
