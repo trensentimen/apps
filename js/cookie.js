bc0062a343f98e48c6c3356b4372ae6318382623
dani ferdinan <daniferdinandall@gmail.com>
2023-10-19 13:24:23 +0700

    update

--- js/cookie.js
+++ js/cookie.js
@@ -1,5 +1,7 @@
 // cookieUtil.js
 
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
 function isCookieExist(cookieName) {
     const cookies = document.cookie.split('; ');
     for (let i = 0; i < cookies.length; i++) {
@@ -11,3 +13,7 @@ function isCookieExist(cookieName) {
     }
     return false;
 }
+
+// getCookie("token");
+
+console.log(getCookie("token"));
