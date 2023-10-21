2bc613e7528383dcdb53cdba817eafbbb5f4b6de
dani ferdinan <daniferdinandall@gmail.com>
2023-10-21 12:32:11 +0700

    update

--- js/postFunctions.js
+++ js/postFunctions.js
@@ -1,7 +1,6 @@
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
-import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
-
+// import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
 export default function PostSignUp() {
     let target_url = "https://asia-southeast2-trens-project.cloudfunctions.net/trensentimen-post";
     // let target_url = "https://asia-southeast2-trens-project.cloudfunctions.net/trensentimen-post2";
@@ -12,7 +11,7 @@ export default function PostSignUp() {
         "password": getValue("password")
     }
 
-    postWithToken(target_url,tokenkey,tokenvalue,datainjson,responseData);
+    postWithToken(target_url, tokenkey, tokenvalue, datainjson, responseData);
 }
 
 
@@ -20,10 +19,18 @@ export default function PostSignUp() {
 
 
 function responseData(result) {
-    
+
     setInner("pesan", result.message);
-    
+
     setCookieWithExpireHour("token", result.token, 2);
     alert("Berhasil Masuk")
     window.location.href = "dashboard.html";
+}
+
+function setCookieWithExpireHour(cname, cvalue, exhour) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exhour * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    
 }
