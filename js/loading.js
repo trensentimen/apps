f52aeac04af94c38a10f2d3f1465af2388150f42
ArykaAnisaP <arykaanisap22@gmail.com>
2023-10-27 20:24:47 +0700

    membuat_halaman_profile

---
+++ js/loading.js
@@ -0,0 +1,13 @@
+// public class ConsoleLoading {
+//     public static void main(String[] args) {
+//         for (int i = 0; i < 10; i++) {
+//             System.out.print("#");
+//             try {
+//                 Thread.sleep(1000); // Jeda 1 detik
+//             } catch (InterruptedException e) {
+//                 e.printStackTrace();
+//             }
+//         }
+//         System.out.println("\nLoading Complete!");
+//     }
+// }
